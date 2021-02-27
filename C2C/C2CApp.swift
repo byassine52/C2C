5e52930d116cf6a08053a411cc5634dc97e92af9
Yassine ElBadaoui <byassine52@users.noreply.github.com>
2021-02-27 23:56:27 +0900

    Initial Commit

---
+++ C2C/C2CApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2CApp.swift
+//  C2C
+//
+//  Created by ياسين البدوي on 2021/02/27.
+//
+
+import SwiftUI
+
+@main
+struct C2CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
